1e4394250ab355d1faa228ea935dec9150e433f2
Luis meza <luimi@live.com>
2025-03-27 15:15:15 -0500

    agregada pagina de estado de servicio

--- index.js
+++ index.js
@@ -1,34 +1,128 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const express = require('express')
+const path = require('path');
+const http = require('http');
+const WebSocket = require('ws');
 require('dotenv').config()
 
 const app = express()
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+const server = http.createServer(app);
+const wsServer = new WebSocket.Server({ server });
+const { PORT } = process.env;
 
-app.listen(process.env.PORT || 3000)
+server.listen(PORT || 3000, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT || 3000}`);
+});
 
-const client = new Client({
+let status = {
+    code: null,
+    status: "Disconnected",
+    service: "Paused"
+}
+const wwClient = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: { headless: true },
 });
-client.on('qr', (qr) => {
-    console.log(`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qr)}&size=800x800&margin=10&ecc=H`);
+
+const sendStatusUpdate = () => {
+    wsServer.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) client.send(JSON.stringify(status));
+    });
+}
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+
+           _____ _____ 
+     /\   |  __ \_   _|
+    /  \  | |__) || |  
+   / /\ \ |  ___/ | |  
+  / ____ \| |    _| |_ 
+ /_/    \_\_|   |_____|
+                       
+                       
+
+*/
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.post('/startService', (req, res) => {
+    wwClient.initialize();
+    res.send('Service Started')
+    sendStatusUpdate();
+})
+
+app.post('/stopService', (req, res) => {
+    wwClient.destroy();
+    status.service = "Paused"
+    status.code = null
+    res.send('Service Stopped')
+    sendStatusUpdate();
+})
+
+app.post('/restartService', (req, res) => {
+    wwClient.destroy();
+    wwClient.initialize();
+    res.send('Service Restarted')
+})
+
+app.post('/disconnectAccount', (req, res) => { 
+    wwClient.logout()
+    status.status = "Disconnected"
+    wwClient.initialize();
+    res.send('Disconnected')
+})
+
+
+
+/*
+
+ __          ___           _                            
+ \ \        / / |         | |         /\                
+  \ \  /\  / /| |__   __ _| |_ ___   /  \   _ __  _ __  
+   \ \/  \/ / | '_ \ / _` | __/ __| / /\ \ | '_ \| '_ \ 
+    \  /\  /  | | | | (_| | |_\__ \/ ____ \| |_) | |_) |
+     \/  \/   |_| |_|\__,_|\__|___/_/    \_\ .__/| .__/ 
+                                           | |   | |    
+                                           |_|   |_|    
+
+*/
+wwClient.on('qr', (qr) => {
+    status.code = qr
+    status.service = "Running"
+    sendStatusUpdate();
 });
-client.on('ready', () => {
-    console.log('Client is ready!');
+wwClient.on('ready', () => {
+    status.status = "Connected"
+    status.service = "Running"
+    status.code = null
+    sendStatusUpdate();
 });
-client.on('message', msg => {
-    if (!msg.fromMe) {
-        console.log("message", msg.body)
-    }
+wwClient.on('message', msg => {
     if (msg.body == '!ping') {
         msg.reply('pong');
     }
 });
-client.on('disconnected', (reason) => {
-    console.log('Client was logged out', reason);
+wwClient.on('disconnected', (reason) => {
+    status.status = "Disconnected"
+    sendStatusUpdate();
 });
 
-client.initialize();
+wwClient.initialize();
+
+/*
+
+ __          __  _     _____            _        _   
+ \ \        / / | |   / ____|          | |      | |  
+  \ \  /\  / /__| |__| (___   ___   ___| | _____| |_ 
+   \ \/  \/ / _ \ '_ \\___ \ / _ \ / __| |/ / _ \ __|
+    \  /\  /  __/ |_) |___) | (_) | (__|   <  __/ |_ 
+     \/  \/ \___|_.__/_____/ \___/ \___|_|\_\___|\__|
+                                                     
+                                                     
+
+*/
+wsServer.on('connection', (ws) => {
+    ws.send(JSON.stringify(status));
+});
